22b8b15efde99d8203869623a97a53fe5cf52ec4
Shalom Sims <>
2022-02-10 00:28:39 -0500

    Wrote 1 or 2 test cases

--- test/weather_test.exs
+++ test/weather_test.exs
@@ -2,8 +2,15 @@ defmodule WeatherTest do
   use ExUnit.Case
   doctest Weather
 
-  test "Returns string" do
-    assert Weather.max_temp()  
+  test "Returns a string value" do
+    assert to_string(Weather.max_temp()), "Single digit number is probably celcius"
   end
 
+  test "Correct C to F conversion" do
+    assert Weather.to_fahrenheit(10) === 50, "Correct conversion."
+  end
+
+  test "I don't know how to test anything else from here right now." do
+    assert true, "It is true."
+  end
 end
